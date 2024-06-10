9f415efda84c22a176f34c2ad6ce501e5fad12bc
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2024-06-10 11:38:39 +0900

    09まですべて終了

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,6 +23,7 @@ public class ItemScript : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         animator.SetTrigger("Get");
+        audioSource.Play();
     }
 
     private void OnTriggerStay(Collider other)
