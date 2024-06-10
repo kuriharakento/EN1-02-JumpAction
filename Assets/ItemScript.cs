55bc452603bae6d101ccbfc4f793386b5997db11
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2024-06-10 11:09:19 +0900

    08はすべて終了

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger("Get");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+}
